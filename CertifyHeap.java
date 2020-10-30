cd495959846680defecd2975c9fa3f9b8d4da809
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-10-30 14:55:34 -0700

    Max Heap Check

--- CertifyHeap.java
+++ CertifyHeap.java
@@ -12,7 +12,11 @@ public class CertifyHeap {
     // otherwise. Remember to index from 1.
     @SuppressWarnings({"rawtypes"})
     private static boolean maxOrderedHeap(Comparable[] a) {
+        // Start at 2 since MaxHeap ignores 0th index.
+        // 1st index is assumed to be the largest.
         for (int i = 2; i < a.length; i++) {
+            // a[i/2] is a parent node, a[i] is child node
+            // MaxHeap property: parent > child
             if (!less(a[i], a[i / 2])) {
                 StdOut.println(i / 2);
                 return false;
